29cfd259c652d63826d6de79e38c3fa720150e7a
Bober3939393 <serhijmaritsan2@gmail.com>
2026-02-21 17:40:39 +0200

    Added background image

--- client.py
+++ client.py
@@ -42,7 +42,7 @@ def receive():
 font_win = font.Font(None, 72)
 font_main = font.Font(None, 36)
 # --- ЗОБРАЖЕННЯ ----
-img = Image.open("4118.jpeg")
+img = Image.open("")
 img = img.resize((WIDTH, HEIGHT))
 
 mode = img.mode
